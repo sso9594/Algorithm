b7b57466ddc5a3de9f696e0d3836eeb878ebe332
SYoung <58455389+sso9594@users.noreply.github.com>
2024-05-07 13:42:16 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.py
@@ -0,0 +1,11 @@
+# i = int(input())
+
+
+# for k in range(1, i+1):
+#     for j in range(k):
+#         print("*", end='')
+#     print()
+
+i = int(input())
+for j in range(1, i+1):
+    print("*" * j)
