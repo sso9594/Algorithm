5373f7d5a09ac982d46e14ec0a5af3bff3c4fd5d
SYoung <58455389+sso9594@users.noreply.github.com>
2024-05-22 18:23:47 +0900

    [Bronze V] Title: 오늘 날짜, Time: 200 ms, Memory: 16812 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.time.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+    }
+}
