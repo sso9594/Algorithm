6a7747f281df9dae74545f6a31ac72c0c86e9b8e
syoung <58455389+sso9594@users.noreply.github.com>
2025-01-27 00:17:22 +0900

    [D3] Title: 농작물 수확하기, Time: 138 ms, Memory: 39,408 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -6,48 +6,39 @@ class Solution
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
-        
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
+        int T = sc.nextInt();
+
+        for(int test_case=1; test_case <= T; test_case++){
             int N = sc.nextInt();
-            int[][] farm = new int[N][N];
+            int[][] arr = new int[N][N];
             int sum = 0;
-            for(int i=0; i<N; i++){
+
+            for (int i = 0; i < N; i++) {
                 String token = sc.next();
-                for(int j=0; j<N; j++){
-                    farm[i][j] = Integer.parseInt(token.charAt(j)+"");
-                }
-            }
-
-            // 위
-            for(int i=0; i<N/2; i++){
-                // 행 오른쪽
-                for(int j=N/2+1; j<=N/2+i; j++){
-                     sum += farm[i][j];
-                }
-                // 행 왼쪽
-                for(int j=N/2; j>=N/2-i; j--){
-                    sum += farm[i][j];
-                }
-            }
-
-            int end = N;
-            // 아래 N=5일때 i=2 부터 시작
-            for(int i=N/2, k=0; i<N; i++, k++){
-                // 행 오른쪽
-                for(int j=N/2; j<N-k; j++){
-                    sum += farm[i][j];
-                }
-               // 행 왼쪽
-                for(int j=k; j<N/2; j++){
-                   sum += farm[i][j];
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = Integer.parseInt(token.charAt(j)+"");
                 }
             }
             
-            System.out.println("#" + test_case + " " + sum);
+            for (int i = 0; i < (N/2)+1; i++) {
+                for (int j = (N/2)+1; j <= (N/2)+i; j++) {
+                    sum += arr[i][j];
+                }
+                for (int j = (N/2); j >= (N/2)-i; j--) {
+                    sum += arr[i][j];
+                }
+            }
 
-		}
+            for (int i = N-1, k=0; i >= (N/2)+1; i--, k++) {
+                for (int j = (N/2)+1; j <= (N/2)+k; j++) {
+                    sum += arr[i][j];
+                }
+                for (int j = (N/2); j >= (N/2)-k; j--) {
+                    sum += arr[i][j];
+                }
+            }
+
+            System.out.println("#" + test_case + " " + sum);
+        }
 	}
 }
