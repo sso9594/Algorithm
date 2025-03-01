a145ae590d213df44de345858e0a095e2feac910
syoung <58455389+sso9594@users.noreply.github.com>
2025-03-01 23:31:06 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 12992 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,30 +1,48 @@
-import java.util.*;
-import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
-public class Main{
-    static int n,m;
+public class Main {
+    static ArrayList<int[]> permuSet;
+    static int N;
+    static int M;
     static int[] arr;
-    public static void main(String[] args)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-        n= Integer.parseInt(st.nextToken());
-        m =Integer.parseInt(st.nextToken());
-        arr = new int[m];
+        N = sc.nextInt();
+        M = sc.nextInt();
 
-        dfs(1,0);
-    }
-    public static void dfs(int a, int depth){
-        if(depth == m){
-            for(int val: arr){
-                System.out.print(val + " ");
+        arr = new int[N];
+        permuSet = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i+1;
+        }
+
+        permu(0, 0, new int[M], new boolean[N]);
+
+        for (int[] set : permuSet) {
+            for (int i = 0; i < set.length; i++) {
+                System.out.print(set[i] + " ");
             }
             System.out.println();
+        }
+    }
+
+    static void permu(int index, int depth, int[] sel, boolean[] visited){
+        if(depth==M){
+            permuSet.add(Arrays.copyOf(sel, depth));
             return;
         }
-        for(int i =a; i<=n; i++){
-                arr[depth] = i;
-                dfs(i+1, depth+1);
+
+        for (int i = index; i < N; i++) {
+            if(!visited[i]){
+                visited[i] = true;
+                sel[depth] = arr[i];
+                permu(i+1, depth+1, sel, visited);
+                visited[i] = false;
+            }
         }
     }
 }
