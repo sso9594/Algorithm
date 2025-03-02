18886b31ff9a96fbc06c050603f1c013ac045cbe
syoung <58455389+sso9594@users.noreply.github.com>
2025-03-03 02:18:08 +0900

    [Silver III] Title: N과 M (5), Time: 300 ms, Memory: 61084 KB -BaekjoonHub

--- 백준/Silver/15654. N과 M （5）/N과 M （5）.java
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.java
@@ -1,46 +1,61 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
-public class Main{
-    static int n,m;
-    static int[] value;
+public class Main {
+    static int N;
+    static int M;
     static int[] arr;
-    static StringBuilder sb = new StringBuilder();
-    static boolean[] visited;
-    public static void main(String[] args) throws IOException{
+    static BufferedWriter bw;
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        n =Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        value = new int[n];
-        arr = new int[m];
-        visited = new boolean[n];
+        arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        for(int i =0; i<n; i++){
-            value[i] = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(value);
 
-        dfs(0);
-        System.out.println(sb);
+        Arrays.sort(arr);
 
+        comb(0, 0, new int[M], new boolean[N]);
+
+        bw.flush();
     }
-    public static void dfs(int depth){
-        if(depth == m){
-            for(int i =0; i<m; i++){
-                sb.append(arr[i]).append(" ");
+
+    static void comb(int index, int depth, int[] sel, boolean[] visited) throws IOException{
+        if(depth == M){
+            for (int i = 0; i < sel.length; i++) {
+                bw.write(sel[i] + " ");
             }
-            sb.append('\n');
+            bw.write("\n");
             return;
         }
-        for(int i = 0; i<n; i++){
+
+        if(index==N){
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
             if(!visited[i]){
-                visited[i]=true;
-                arr[depth] =value[i];
-                dfs(depth+1);
+                visited[i] = true;
+                sel[depth] = arr[i];
+                comb(i, depth+1, sel, visited);
                 visited[i] = false;
             }
         }
+        
     }
 }
