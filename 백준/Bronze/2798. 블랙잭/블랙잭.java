f773e322f0f0ee6539df2f5c9ced04e2987841dc
syoung <58455389+sso9594@users.noreply.github.com>
2025-03-04 13:35:47 +0900

    [Bronze II] Title: 블랙잭, Time: 116 ms, Memory: 13276 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -1,42 +1,47 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+	
+	static int N;
+	static int M;
+	static int[] arr;
+	static int result;
+	public static void main(String[] args){
+		//--------------솔루션 코드를 작성하세요.--------------------------------
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		result = Integer.MIN_VALUE;
+		
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		comb(0, 0, new int[3]);
+		System.out.println(result);
+	}
+	
+	static void comb(int index, int depth, int[] sel) {
+		if(depth == 3) {
+			int sum = 0;
+			for (int i = 0; i < sel.length; i++) {
+				sum += sel[i];
+			}
+			if(sum<=M) {
+				result = Math.max(result, sum);
+			}
+			return;
+		}
+		
+		if(index==N) {
+			return;
+		}
+		
+		sel[depth] = arr[index];
+		comb(index+1, depth+1, sel);
+		comb(index+1, depth, sel);
+	}
 
-    static int[] card;
-    static int N;
-    static int M;
-    static int max;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        N = sc.nextInt();
-        M = sc.nextInt();
-
-        card = new int[N];
-        for (int i = 0; i < N; i++) {
-            card[i] = sc.nextInt();
-        }
-
-        max = 0;
-
-        comb(0, 0, 0);
-
-        System.out.println(max);
-    }
-
-    static void comb(int index, int sum, int depth){
-        if (depth == 3) { 
-            if (sum <= M) {
-                max = Math.max(sum, max);
-            }
-            return;
-        }
-    
-        if (index >= N) {
-            return;
-        }
-
-        comb(index+1, sum+card[index], depth+1);
-        comb(index+1, sum, depth);
-    }
 }
