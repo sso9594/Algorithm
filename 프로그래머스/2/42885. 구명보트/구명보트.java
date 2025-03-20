b0acc7cf8f7829eee5d8ee8a0877c1b4a490bf43
syoung <58455389+sso9594@users.noreply.github.com>
2025-03-21 00:38:01 +0900

    [level 2] Title: 구명보트, Time: 11.31 ms, Memory: 58.6 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -4,17 +4,17 @@ class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        
         int left = 0;
         int right = people.length-1;
-        
-        while(left<=right){
-            if(people[left] + people[right] <= limit){
+        while(left <= right){
+            if(people[left]+people[right] <= limit){
+                answer++;
                 left++;
+                right--;
+            } else{
+                answer++;
+                right--;
             }
-            
-            right--;
-            answer++;
         }
         
         return answer;
