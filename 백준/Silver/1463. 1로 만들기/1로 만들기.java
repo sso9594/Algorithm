813d1581ccba76731bec6a7843ba4fba4fbb07a8
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-04 12:59:46 +0900

    [Silver III] Title: 1로 만들기, Time: 104 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,25 +1,34 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int count = 0;
-        int[] dp = new int[n+1];
-        dp[0] = dp[1] = 0;
 
-        for(int i=2; i<=n; i++){
-            dp[i] = dp[i-1] + 1;
-            if(i%2==0){
-                dp[i] = Math.min(dp[i], dp[i/2] + 1);
-            }
-            if(i%3==0){
-                dp[i] = Math.min(dp[i], dp[i/3] + 1);
-            }
-        }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
 
-        System.out.println(dp[n]);
-    }
+		int[] dp = new int[N+1];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[1] = 0;
+		for (int i = 2; i < N+1; i++) {
+			if(i%2==0) dp[i] = Math.min(dp[i],dp[i/2] + 1);
+			if(i%3==0) dp[i] = Math.min(dp[i], dp[i/3] + 1);
 
+			dp[i] = Math.min(dp[i], dp[i-1]+1);
+		}
+
+		System.out.println(dp[N]);
+	}
 }
+
+/*
+ * dp[3] = 1
+ * dp[2] = 1
+ * dp[1] = 1
+ * 
+ * dp[4] = Math.min(dp[3] + dp[1], dp[2] + dp[2]) = 2
+ * dp[5] = Math.min(dp[4] + dp[1], dp[1] + dp[1] + dp[3]) = 3
+	dp[n] 이 2로 나눠지면 비교 해보고 3으로 나눠지면 비교해보고 안되면 그냥 이전 값의 + 1
+ */
