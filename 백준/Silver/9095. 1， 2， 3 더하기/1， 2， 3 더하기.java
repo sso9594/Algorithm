a71d0e321cf52e8a7232f9c7903249c2a29daf55
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-04 13:43:50 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 11436 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,44 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
-        ArrayList<Integer> testCase = new ArrayList<>();
 
-        for(int i=0; i<T; i++){
-            testCase.add(Integer.parseInt(br.readLine()));
-        }
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
 
-        for(int test : testCase){
-            System.out.println(dp(test));
-        }
-    }
+		int[] dp = new int[11];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 4;
 
-    static int dp(int num){
-        int[] dp = new int[num+4];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 4;
-        if(num<4) return dp[num];
-        for(int i=4; i<=num; i++){
-            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
-        }
-        return dp[num];
-    }
+		for (int i = 4; i < 11; i++) {
+			dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+		}
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			System.out.println(dp[N]);
+		}
 
-    /*
-     * dp[0] = 0
-     * dp[1] = 1 (1까지 합 방법의 갯수)
-     * dp[2] = 1+1, 2 = 2
-     * dp[3] = 1+1+1, 1+2, 2+1, 3 = 4
-     * dp[4] = 1+1+1+1, 1+1+2, 1+2+1, 2+1+1, 2+2, 1+3, 3+1 = 7
-     * dp[5] = 1+1+1+1+1, 1+1+1+2, 1+1+2+1, 1+2+1+1, 2+1+1+1, 2+2+1, 2+1+2, 1+2+2, 3+1+1, 1+3+1, 1+1+3
-     *         2+3, 3+2 = 13
-     * if(i>=4)
-     *     dp[i] = dp[i-1] + dp[i-1] + dp[i-3];
-     * 
-     */
+		
+	}
 }
+
+/*
+ * dp[1] = 1
+ * dp[2] = 2
+ * dp[3] = 4
+ * dp[4] = 7
+ * dp[5] = dp[4] + dp[3] + dp[2]
+
+ * dp[N] = 
+ */
