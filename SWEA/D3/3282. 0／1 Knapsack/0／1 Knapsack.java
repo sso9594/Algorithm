ca344ae8063f34e781da56ccd013253dde7f1f6f
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-07 14:36:58 +0900

    [D3] Title: 0/1 Knapsack, Time: 114 ms, Memory: 32,580 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -1,41 +1,38 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-	
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		int T;
-		T=sc.nextInt();
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
 
-		for(int test_case = 1; test_case <= T; test_case++)
-		{
-			int N = sc.nextInt(); // 물건 갯수
-			int K = sc.nextInt(); // 부피합 제한
-			
-			int[][] bag = new int[N+1][2];
-			
-			for(int i=0; i<N; i++) {
-				bag[i][0] = sc.nextInt(); // 부피
-				bag[i][1] = sc.nextInt(); // 가치
-			}
-			
-			int[] dp = new int[K+1];
-			
-			for(int i=0; i<N; i++) {
-				int weight = bag[i][0];
-				int value = bag[i][1];
-				
-				for(int w=K; w>=weight; w--) {
-					dp[w] = Math.max(dp[w], dp[w-weight] + value); 
-				}
-			}
-			
-			System.out.println("#" + test_case+ " " + dp[K]);
-			
-		}
-	}
-	
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken()); // 물건 개수
+            int K = Integer.parseInt(st.nextToken()); // 가방 최대 부피
 
+            int[][] things = new int[N + 1][2];
+            for (int i = 1; i <= N; i++) {
+                st = new StringTokenizer(br.readLine());
+                things[i][0] = Integer.parseInt(st.nextToken()); // 부피
+                things[i][1] = Integer.parseInt(st.nextToken()); // 가치
+            }
+
+            int[][] dp = new int[N + 1][K + 1];
+
+            for (int i = 1; i <= N; i++) {
+                for (int j = 0; j <= K; j++) {
+                    if (things[i][0] > j) {
+                        dp[i][j] = dp[i - 1][j];
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - things[i][0]] + things[i][1]);
+                    }
+                }
+            }
+
+            System.out.println("#" + test_case + " " + dp[N][K]);
+        }
+    }
 }
