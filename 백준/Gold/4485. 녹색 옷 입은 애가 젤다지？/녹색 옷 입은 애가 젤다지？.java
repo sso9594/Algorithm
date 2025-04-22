65718d9fed62e7d7fcc1edba91edcf1a404553c7
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-22 15:36:15 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 480 ms, Memory: 41792 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,45 +1,49 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] map;
-    static int N;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
     static class Node implements Comparable<Node>{
-        int x, y, coin;
-        Node(int x, int y, int coin){
+        int x, y, w;
+
+        Node(int x, int y, int w){
             this.x = x;
             this.y = y;
-            this.coin = coin;
+            this.w = w;
         }
 
         @Override
         public int compareTo(Main.Node o) {
-            return Integer.compare(o.coin, this.coin);
+            return Integer.compare(this.w, o.w);
         }
     }
+    static int N;
+    static int[][] map;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = 1;
+        int test_case = 0;
         while (true) {
             N = Integer.parseInt(br.readLine());
             if(N==0) break;
-
             map = new int[N][N];
-
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
+
             int result = daijkstra(0,0);
 
-            System.out.println("Problem " + count++ + ": " + result);
+            System.out.println("Problem " + ++test_case +": "+ result);
         }
-        
-        
     }
 
     static int daijkstra(int x, int y) {
@@ -49,8 +53,8 @@ public class Main {
         for (int i = 0; i < N; i++) {
             Arrays.fill(dist[i], Integer.MAX_VALUE);
         }
-        dist[0][0] = map[x][y];
-        
+        dist[x][y] = map[x][y];
+
         while (!queue.isEmpty()) {
             Node cur = queue.poll();
             int cx = cur.x;
@@ -59,7 +63,8 @@ public class Main {
             for (int i = 0; i < dx.length; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                if(nx>=0 && ny>=0 && nx<N && ny<N && 
+
+                if(nx>=0 && ny>=0 && nx<N && ny<N &&
                 dist[nx][ny] > dist[cx][cy] + map[nx][ny]){
                     dist[nx][ny] = dist[cx][cy] + map[nx][ny];
                     queue.offer(new Node(nx, ny, map[nx][ny]));
