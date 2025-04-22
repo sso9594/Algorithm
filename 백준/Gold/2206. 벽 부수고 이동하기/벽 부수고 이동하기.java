7a7d70d4fb5f9773ed573c9b81f54ca2c3beff72
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-22 11:49:05 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 832 ms, Memory: 145264 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,86 +1,71 @@
-import java.util.*;
-import java.util.stream.IntStream;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N;
-	static int M;
-	static int min;
-	static int[][] map;
-	static boolean[][][] visited;
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-    public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
+    static int N, M;
+    static int[][] map;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		map = new int[N][M];
-		min = Integer.MAX_VALUE;
-		visited = new boolean[N][M][2];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-		for (int i = 0; i < N; i++) {
-			String token = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = Integer.parseInt(token.charAt(j)+"");
-			}
-		}
+        map = new int[N][M];
 
-		bfs(0, 0);
+        for (int i = 0; i < N; i++) {
+            String token = br.readLine();
+            for (int j = 0; j < M; j++) {
+                map[i][j] = Integer.parseInt(token.charAt(j)+"");
+            }
+        }
 
-		System.out.println(min == Integer.MAX_VALUE ? -1 : min);
-	}
+        int result = bfs(0,0);
 
-	// 1 1
-	// 0
-	// 테케에 대한 처리가 필요
+        System.out.println(result);
+    }
+    static int bfs(int x, int y) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{x, y, 0, 1});
+        boolean[][][] visited = new boolean[N][M][2];
+        visited[x][y][0] = true;
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            int count = cur[2];
+            int dist = cur[3];
 
-	static void bfs(int x, int y){
-		Queue<int[]> queue = new LinkedList<>();
-		// 3번째 원소는 벽을 깰 수 있는 기회
-		queue.offer(new int[]{x, y, 1, 1});
-		visited[x][y][1] = true;
-		visited[x][y][0] = true;
+            if(cx == N-1 && cy == M-1){
+                return dist;
+            }
 
-		if(x==N-1 && y==M-1){
-			min = 1;
-		}
+            for (int i = 0; i < dx.length; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+                // 일반 이동
+                if(nx>=0 && ny>=0 && nx<N && ny<M && !visited[nx][ny][count]
+                && map[nx][ny] == 0){
+                    queue.offer(new int[]{nx, ny, count, dist+1});
+                    visited[nx][ny][count] = true;
+                }
+                //벽 부수고 이동
+                if(count < 1){
+                    if(nx>=0 && ny>=0 && nx<N && ny<M && !visited[nx][ny][count+1]
+                    && map[nx][ny] == 1){
+                        queue.offer(new int[]{nx, ny, count+1, dist+1});
+                        visited[nx][ny][count+1] = true;
+                    }
+                }
+            }
+        }
 
-		while (!queue.isEmpty()) {
-			int[] current = queue.poll();
-			int currentX = current[0];
-			int currentY = current[1];
-			int breakCount = current[2];
-			int distance = current[3];
-			for (int i = 0; i < dx.length; i++) {
-				int nextX = currentX + dx[i];
-				int nextY = currentY + dy[i];
-				int nextCount = breakCount;
-				int nextDistance = distance+1;
-				// 벽 깰 기회가 남아있고, 벽을 만났을 경우
-				if(breakCount>0 && nextX>=0 && nextY>=0 && nextX<N && nextY<M && !visited[nextX][nextY][1]
-				&& map[nextX][nextY]==1){
-					queue.offer(new int[]{nextX, nextY, nextCount-1, nextDistance});
-					visited[nextX][nextY][0] = true;
-				// 벽 깰 기회가 남아있고, 벽이 없을 경우
-				} else if(breakCount > 0 && nextX>=0 && nextY>=0 && nextX<N && nextY<M && !visited[nextX][nextY][1]
-				&& map[nextX][nextY]==0){
-					queue.offer(new int[]{nextX, nextY, nextCount, nextDistance});
-					visited[nextX][nextY][1] = true;
-					if(nextX==N-1 && nextY==M-1){
-						min = Math.min(min, nextDistance);
-					}
-				} else if(nextX>=0 && nextY>=0 && nextX<N && nextY<M && !visited[nextX][nextY][0]
-				&& map[nextX][nextY]==0){
-					queue.offer(new int[]{nextX, nextY, nextCount, nextDistance});
-					visited[nextX][nextY][0] = true;
-					if(nextX==N-1 && nextY==M-1){
-						min = Math.min(min, nextDistance);
-					}
-				}
-			}
-		}
-	}
+        return -1;
+    }
 }
-
