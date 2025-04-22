56bcc66c9bc6a393159500c74d2162e2ce9071eb
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-23 01:19:45 +0900

    [level 2] Title: 전화번호 목록, Time: 330.73 ms, Memory: 210 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -4,16 +4,18 @@ class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
         Map<String, Integer> map = new HashMap<>();
-        for(String token : phone_book){
-            map.put(token, 1);
+        for(int i=0; i<phone_book.length; i++){
+            map.put(phone_book[i], 1);
         }
         for(String token : phone_book){
-            for(int i=1; i<token.length(); i++){
+            for(int i=0; i<token.length(); i++){
                 if(map.containsKey(token.substring(0,i))){
-                    return false;
+                    answer = false;
+                    break;
                 }
             }
         }
+        
         return answer;
     }
 }
