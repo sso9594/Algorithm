8306cc8936d588eeba3d2f8f4b07913ec8389b95
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-23 23:30:07 +0900

    [level 2] Title: 더 맵게, Time: 1663.38 ms, Memory: 128 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -4,20 +4,23 @@ class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> queue = new PriorityQueue<>();
-        for(int num : scoville){
-            queue.offer(num);
+        for(int i=0; i<scoville.length; i++){
+            queue.offer(scoville[i]);
         }
-        while(queue.peek()<K){
-            int fLow = queue.poll();
-            int sLow = queue.poll();
-            int result = fLow + (sLow*2);
-            queue.offer(result);
+        
+        while(queue.peek() < K){
+            if(queue.size() < 2){
+                answer = -1;
+                break;
+            }
+            int min = queue.poll();
+            int sec = queue.poll();
+            
+            int mix = min + (sec * 2);
+            queue.offer(mix);
             answer++;
-            if(queue.size()<=1) break;
         }
-        if(queue.peek()<K){
-            answer = -1;
-        }
+        
         return answer;
     }
 }
