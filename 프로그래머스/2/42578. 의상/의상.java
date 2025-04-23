5589212d23dea79339e2ffee522d21fa9a6dbef1
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-23 22:24:20 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,15 +4,13 @@ class Solution {
     public int solution(String[][] clothes) {
         int answer = 1;
         Map<String, Integer> map = new HashMap<>();
-        for(String[] item : clothes){
-            map.put(item[1], map.getOrDefault(item[1], 0) + 1);
+        for(String[] clothe : clothes){
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
         }
-        
         
         for(int value : map.values()){
             answer *= value + 1;
         }
-        
         return answer-1;
     }
 }
