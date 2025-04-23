f51dc04aab44eedc9d8bd82e104474fd92e258ee
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-23 23:09:48 +0900

    [level 2] Title: 주식가격, Time: 38.79 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -3,21 +3,21 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack();
-        // 들어온 수가 크면 스택에 넣고 작으면 클 때까지 전부 pop
+        Arrays.fill(answer, -1);
+        Stack<Integer> stack = new Stack<>();
         for(int i=0; i<prices.length; i++){
             while(!stack.isEmpty() && prices[i] < prices[stack.peek()]){
-                int index = stack.pop();
-                answer[index] = i - index;
+                int top = stack.pop();
+                answer[top] = i-top;
             }
             stack.push(i);
         }
-        
+
+        int idx = 0;
         while(!stack.isEmpty()){
-            int index = stack.pop();
-            answer[index] = prices.length - index- 1;
+            int top = stack.pop();
+            answer[top] = prices.length - 1 - top;
         }
-        
         return answer;
     }
 }
