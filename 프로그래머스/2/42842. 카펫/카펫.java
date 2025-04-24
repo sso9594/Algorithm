8bc85a4b80b84f40b590b6f2e9ed31d5785e8d17
syoung <58455389+sso9594@users.noreply.github.com>
2025-04-24 15:24:38 +0900

    [level 2] Title: 카펫, Time: 0.16 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,31 +1,35 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        // yellow의 가로 = brown의 가로 - 2 => brown 가로 = yellow 가로 + 2
-        // yellow의 세로 = brown의 세로 - 2 => brown 세로 = yellow 세로 + 2
-        int col = 0;
-        int row = 0;
-        
-        for(int i = 1; i<=yellow; i++){
-            int current_w = 0;
+        List<Integer> list = new ArrayList<>();
+        // yellow의 약수 구하기
+        for(int i=1; i<=Math.sqrt(yellow); i++){
+            if(yellow%i==0) list.add(i);
+        }
+        // 곱해서 yellow가 나오는 약수 묶음 구하기
+        // 두 수 * 2 - 4 = brown이 나오는 값 구하기
+        for(int i=0; i<list.size(); i++){
+            int num1 = list.get(i);
+            int num2 = yellow / num1;
             
-            if((yellow%i == 0)){
-                current_w = (int)(yellow/i)+2;
+            int cal = (num1 + num2+4) * 2 - 4;
+            if(cal == brown){
+                answer[0] = Math.max(num1+2, num2+2);
+                answer[1] = Math.min(num1+2, num2+2);
             }
-            int current_h = i+2;
-            
-            if(((current_w * 2) + (current_h - 2) * 2) == brown){
-                if(current_w >= current_h){
-                    answer[0] = current_w;
-                    answer[1] = current_h;
-                }else{
-                    answer[0] = current_h;
-                    answer[1] = current_w;
-                }
-                break;
-            } 
-            
         }
         return answer;
     }
 }
+/*
+1 2 3 4 6 8 12 24
+
+3 26 => 29 * 2 - 4 = 54
+4 14 => 18 * 2 - 4 = 32
+5 10 => 15 * 2 - 4 = 26 
+6 8 =>  14 * 2 - 4 = 24 => brown
+
+
+*/
