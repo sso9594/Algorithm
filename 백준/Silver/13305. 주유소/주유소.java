87a2bc53c8c258ccbef361f8471e642b8ec0e269
syoung <58455389+sso9594@users.noreply.github.com>
2025-07-09 09:57:45 +0900

    [Silver III] Title: 주유소, Time: 264 ms, Memory: 41456 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,31 +8,33 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int answer = 0;
+        long answer = 0;
 
-        int[] road = new int[N-1];
-        int[] gas = new int[N];
+        long[] road = new long[N-1];
+        long[] gas = new long[N];
         
         StringTokenizer st = new StringTokenizer(br.readLine());
         StringTokenizer st2 = new StringTokenizer(br.readLine());
         for (int i = 0; i < road.length; i++) {
-            road[i] = Integer.parseInt(st.nextToken());
-            gas[i] = Integer.parseInt(st2.nextToken());
+            road[i] = Long.parseLong(st.nextToken());
+            gas[i] = Long.parseLong(st2.nextToken());
         }
 
-        gas[gas.length-1] = Integer.parseInt(st2.nextToken());
+        gas[gas.length-1] = Long.parseLong(st2.nextToken());
 
         for (int i = 0; i < road.length; i++) {
             answer += gas[i] * road[i];
             int current = i;
+            long sum = 0;
             for (int j = i+1; j < road.length; j++) {
                 if(gas[i] < gas[j]){
-                    answer += gas[i] * road[j];
+                    sum += road[j];
                     current = j;
                 } else {
                     break;
                 }
             }
+            answer += gas[i] * sum;
             i = current;
         }
 
@@ -49,5 +48,3 @@ public class Main {
  * 현재 위치의 주유소에서 다음 주유소가 자기 보다 낮은 가격일 때 까지의
  * 거리를 전부 구매
  */
-
- 
