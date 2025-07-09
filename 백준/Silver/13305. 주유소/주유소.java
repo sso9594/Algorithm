378ff9a4d9d6833f9faff6e5daf80e20afc93847
syoung <58455389+sso9594@users.noreply.github.com>
2025-07-09 09:30:20 +0900

    [Silver III] Title: 주유소, Time: 1860 ms, Memory: 31096 KB, Score: 17 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int answer = 0;
+
+        int[] road = new int[N-1];
+        int[] gas = new int[N];
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st2 = new StringTokenizer(br.readLine());
+        for (int i = 0; i < road.length; i++) {
+            road[i] = Integer.parseInt(st.nextToken());
+            gas[i] = Integer.parseInt(st2.nextToken());
+        }
+
+        gas[gas.length-1] = Integer.parseInt(st2.nextToken());
+
+        for (int i = 0; i < road.length; i++) {
+            answer += gas[i] * road[i];
+            for (int j = i+1; j < road.length; j++) {
+                if(gas[i] < gas[j]){
+                    answer += gas[i] * road[j];
+                    i=j;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
+
+/**
+ * [5, 2, 4]
+ * 
+ * 현재 위치의 주유소에서 다음 주유소가 자기 보다 낮은 가격일 때 까지의
+ * 거리를 전부 구매
+ */
