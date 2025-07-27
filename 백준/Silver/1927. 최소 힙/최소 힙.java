b9c5d7696b3cf871e0c3ca336f38c1d72f6334f7
syoung <58455389+sso9594@users.noreply.github.com>
2025-07-27 11:33:53 +0900

    [Silver II] Title: 최소 힙, Time: 268 ms, Memory: 28364 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -1,23 +1,30 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        PriorityQueue<Integer> heap = new PriorityQueue<>();
-        for(int i=0; i<N; i++){
-            int x = Integer.parseInt(br.readLine());
-            if(x==0){
-                if(heap.size()>0){
-                    System.out.println(heap.poll());
-                } else{
-                    System.out.println(0);
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < N; i++) {
+            int cmd = Integer.parseInt(br.readLine());
+            if(cmd == 0){
+                if(pq.size() > 0){
+                    sb.append(pq.poll()+"\n");
+                } else {
+                    sb.append(0  + "\n");
                 }
-            } else if (x>0){
-                heap.add(x);
+            } else {
+                pq.offer(cmd);
             }
         }
 
+        sb.deleteCharAt(sb.length()-1);
+        System.out.println(sb.toString());
     }
 }
