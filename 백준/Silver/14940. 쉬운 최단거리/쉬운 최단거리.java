97b400adf1c08c29c3e291c2f756d53fdb5f0664
syoung <58455389+sso9594@users.noreply.github.com>
2025-08-06 21:55:18 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 480 ms, Memory: 49572 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -1,84 +1,78 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int N;
-	static int M;
-	static int[][] map;
-	static int[][] distance;
-	static boolean[][] visited;
-	//상하좌우
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1,1};
-	
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		distance = new int[N][M];
-		visited = new boolean[N][M];
-		
-		int[] start = new int[2];
-		
-		for(int i=0; i<N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int j=0; j<M; j++) {
-				int temp = Integer.parseInt(st.nextToken());
-				map[i][j] = temp;
-				if(temp==2) {
-					start[0] = i;
-					start[1] = j;
-				}
-                distance[i][j] = -1;
-			}
-		}
-		
-		bfs(start[0], start[1]);
-		
-		
-		for(int i=0; i<N; i++) {
-			for(int j=0; j<M; j++) {
-				if(map[i][j]==0) {
-					distance[i][j] = 0; 
-				}
-				System.out.print(distance[i][j] + " ");
-			}
-			System.out.println("");
-		}
-		
-		
-		
-	}
-	
-	static void bfs(int x, int y) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[] {x,y});
-		visited[x][y] = true;
-		distance[x][y] = 0; 
-		int length = 0;
-		
-		while(!queue.isEmpty()) {
-			int[] current = queue.poll();
-			int currentX = current[0];
-			int currentY = current[1];
-			int currentDis = distance[currentX][currentY];
-			
-			for(int i=0; i<dx.length; i++) {
-				int nextX = currentX + dx[i];
-				int nextY = currentY + dy[i];
-				if(nextX>=0 && nextY>=0 && nextX<N && nextY<M && !visited[nextX][nextY]
-						&& map[nextX][nextY]==1) {
-					queue.offer(new int[] {nextX, nextY});
-					distance[nextX][nextY] = currentDis + 1;
-					visited[nextX][nextY] = true;
-				}
-			}
-		}
-	}
-	
+    static int n;
+    static int m;
+    static int[][] map;
+    static int[] dx = {-1,1,0,0};
+    static int[] dy = {0,0,-1,1};
+    static boolean[][] visited;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
+        int startX = 0, startY = 0;
+        visited = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if(map[i][j] == 2){
+                    startX = i;
+                    startY = j;
+                }
+            }
+        }
+
+        bfs(startX, startY);
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if(map[i][j]!=0 && !visited[i][j]){
+                    sb.append(-1);
+                } else {
+                    sb.append(map[i][j]);
+                }
+                if(j < m-1) sb.append(" ");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb.toString());
+    }
+    static void bfs(int startX, int startY) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{startX, startY, 0});
+        visited[startX][startY] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+            int count = cur[2];
+            
+            map[cx][cy] = count;
+
+            for (int i = 0; i < dx.length; i++) {
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx>=0 && ny>=0 && nx<n && ny<m
+                && !visited[nx][ny] && map[nx][ny]==1){
+                    queue.offer(new int[]{nx, ny, count+1});
+                    visited[nx][ny] = true;
+                }
+            }
+        }
+    }
 }
