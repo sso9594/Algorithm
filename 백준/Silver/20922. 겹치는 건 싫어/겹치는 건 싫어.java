dff0ee315eb44dd7d8738224935dd44625c0676c
syoung <58455389+sso9594@users.noreply.github.com>
2025-08-08 00:10:37 +0900

    [Silver I] Title: 겹치는 건 싫어, Time: 512 ms, Memory: 67644 KB -BaekjoonHub

---
+++ 백준/Silver/20922. 겹치는 건 싫어/겹치는 건 싫어.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        int answer = 0;
+        int left = 0;
+
+        for (int i = 0; i < N; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0)+1);
+            if(map.get(arr[i]) > K){
+                while (map.get(arr[i]) > K) {
+                    map.put(arr[left], map.get(arr[left])-1);
+                    left++;
+                }
+            }
+            int len = i - left + 1;
+            answer = Math.max(answer, len);
+        }
+
+        System.out.println(answer);
+    }
+}
+/**
+ * 9 2
+ * 3 2 5 5 6 4 4 5 7
+ * 
+ * 
+ * 7
+ */
