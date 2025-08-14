5eb0875082c86adc335c25a9eb4bbd20aa1fed52
syoung <58455389+sso9594@users.noreply.github.com>
2025-08-15 00:12:40 +0900

    [Silver I] Title: 숨바꼭질, Time: 104 ms, Memory: 14876 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,58 +1,55 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Map;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	static int min;
-	static boolean[] visited;
-	static int K;
-	static int N;
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		min = Integer.MIN_VALUE;
-		visited = new boolean[200001];
-		
-		// 시작점 N
-		// 도착점 K
-		// 할 수 있는 행동
-		// 1. N+1, 2. N-1, 3. 2*N
-		bfs(N);
-		System.out.println(min);
-		
-	}
-	
-	static void bfs(int num) {
-		Queue<int[]> queue = new LinkedList<>();
-		queue.offer(new int[]{num, 0});
-		visited[num] = true;
-		
-		while(!queue.isEmpty()) {
-			int[] current = queue.poll();
-			int position = current[0];
-			int time = current[1];
-			
-			if(position==K) {
-				min = time;
-				return;
-			}
-			
-			int[] next = new int[] {position+1, position-1, position*2};
-			for(int doNext : next) {
-				if(doNext>=0 && !visited[doNext] && doNext <= 100000) {
-					visited[doNext] = true;
-					queue.offer(new int[] {doNext, time+1});
-				}
-			}
-			
-		}
-		
-	}
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int answer = 0;
+
+        int[] dx = {-1, 1, 2};
+
+        Queue<int[]> queue = new ArrayDeque<>();
+        queue.offer(new int[]{N, 0});
+        boolean[] visited = new boolean[100_001];
+        visited[N] = true;
+
+        while (!queue.isEmpty()) {
+            int[] cur = queue.poll();
+            int curNum = cur[0];
+            int count = cur[1];
+
+            if(curNum == K){
+                answer = count;
+                break;
+            }
+
+            for (int i = 0; i < dx.length; i++) {
+                int nextNum = 0;
+                if(i != dx.length-1){
+                    nextNum = curNum + dx[i];
+                } else {
+                    nextNum = curNum * dx[i];
+                }
+
+                if(nextNum > 100_000) continue;
+                if(nextNum < 0) continue;
+
+                if (!visited[nextNum]) {
+                    queue.offer(new int[]{nextNum, count+1});
+                    visited[nextNum] = true;
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
 }
