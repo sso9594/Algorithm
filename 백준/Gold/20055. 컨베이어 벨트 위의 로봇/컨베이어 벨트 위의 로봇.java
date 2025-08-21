6d02c3bb3efde803fd73ca05f6158739af268082
syoung <58455389+sso9594@users.noreply.github.com>
2025-08-21 12:36:35 +0900

    [Gold V] Title: 컨베이어 벨트 위의 로봇, Time: 216 ms, Memory: 13384 KB -BaekjoonHub

--- 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
+++ 백준/Gold/20055. 컨베이어 벨트 위의 로봇/컨베이어 벨트 위의 로봇.java
@@ -1,82 +1,73 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] conveyor;
-    static int[] robots;
-    static int N;
-    static int K;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    static int count;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = sc.nextInt();
-        K = sc.nextInt();
+        int N = Integer.parseInt(st.nextToken()); 
+        int K = Integer.parseInt(st.nextToken());
 
-        conveyor = new int[2*N];
-        robots = new int[N];
-        int count = 0;
-
-        for (int i = 0; i < conveyor.length; i++) {
-            conveyor[i] = sc.nextInt();
-        }
-        
-        while (!checkBreakpoint()) {
-            rotateConveyor();
-            moveRobots();
-            raiseRobot();
-            count++;
+        st = new StringTokenizer(br.readLine());
+        int[] belt = new int[2*N];
+        int[] robots = new int[N];
+        for (int i = 0; i < 2 * N; i++) {
+            belt[i] = Integer.parseInt(st.nextToken());
         }
 
-        System.out.println(count);
-    }
-    static void raiseRobot(){
-        if(conveyor[0]!=0){
-            robots[0] = 1;
-            conveyor[0]--;
+        count = 0;
+        int answer = 0;
+
+        while (count < K) {
+            answer++;
+            rotateBelt(belt, robots);
+            moveRobots(belt, robots);
+            if(belt[0] > 0 && robots[0] == 0){
+                robots[0] = 1;
+                belt[0]--;
+            }
+            checkBelt(belt);        
         }
+
+        System.out.println(answer);
     }
 
-    static void moveRobots(){
-        for (int i = robots.length-1; i >= 0; i--) {
-            // 로봇이 있을 경우
-            if(robots[i]==1){
-                // 로봇이 밟을 칸이 1 이상일 경우
-                if(conveyor[i+1] >= 1 && robots[i+1]!=1){
-                    // 로봇을 전진하고 컨베이어 -1
-                    robots[i+1] = robots[i];
+    static void checkBelt(int[] belt) {
+        int count0 = 0;
+        for (int i = 0; i < belt.length; i++) {
+            if(belt[i] == 0){
+                count0++;
+            }
+        }
+        count = count0;
+    }
+
+    static void moveRobots(int[] belt, int[] robots) {
+        for (int i = robots.length-2; i >=0; i--) {
+            if(robots[i]>0){
+                if(robots[i] == 1 && robots[i + 1] == 0 && belt[i+1]>0){
+                    robots[i+1] = 1;
+                    belt[i+1]--;
                     robots[i] = 0;
-                    conveyor[i+1]--;
                 }
             }
         }
-
-        if(robots[robots.length-1]==1){
-            robots[robots.length-1] = 0;
-        }
     }
 
-    static void rotateConveyor(){
-        int temp = conveyor[conveyor.length-1];
-        for (int i = conveyor.length-1; i > 0; i--) {
-            conveyor[i] = conveyor[i-1];
+    static void rotateBelt(int[] belt, int[] robots) {
+        int last = belt[belt.length-1];
+        for (int i = belt.length-2; i >=0; i--) {
+            belt[i+1] = belt[i];
         }
-        for (int i = robots.length-1; i>=1; i--) {
-            robots[i] = robots[i-1];
-            robots[i-1] = 0;
+        for (int i = robots.length-2; i >= 0; i--) {
+            robots[i+1] = robots[i];
         }
-        conveyor[0] = temp;
-        if(robots[robots.length-1]==1){
-            robots[robots.length-1] = 0;
-        }
-    }
-
-    static boolean checkBreakpoint(){
-        int count = 0;
-        for (int i = 0; i < conveyor.length; i++) {
-            if(conveyor[i] == 0){
-                count++;
-            }
-        }
-        if(count >= K) return true;
-        else return false;
+        belt[0] = last;
+        robots[0] = 0;
+        robots[robots.length-1] = 0;
     }
 }
