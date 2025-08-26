4d1e4963c4b97fa2fbccdbc694ccaa1838e1485d
syoung <58455389+sso9594@users.noreply.github.com>
2025-08-26 23:57:11 +0900

    [Gold V] Title: 탑, Time: 2628 ms, Memory: 187012 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,43 +1,44 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
-        int n = Integer.parseInt(br.readLine());
-        
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[n];
-        for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        
-        int[] result = new int[n];
+        int N =  Integer.parseInt(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         Stack<int[]> stack = new Stack<>();
-        
-        for (int i = 0; i < n; i++) {
-            int num = arr[i];
-            
-            while (!stack.isEmpty() && stack.peek()[1] <= num) {
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(st.nextToken());
+
+            while (!stack.isEmpty() && stack.peek()[1] < num) {
                 stack.pop();
             }
             
-            if (!stack.isEmpty()) {
-                result[i] = stack.peek()[0] + 1;
+            if(stack.isEmpty()){
+                arr[i] = 0;
+            } else{
+                arr[i] = stack.peek()[0] + 1;
             }
-            
+
             stack.push(new int[]{i, num});
         }
-        
-        for (int i = 0; i < n; i++) {
-            bw.write(result[i]+ " ");
-        }        
 
-        bw.flush();
-        br.close();
-        bw.close();
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
     }
 }
+
+/**
+ * 9 4 6 3 7 8
+ * 9: stack 비어 있으니 삽입
+ * 4: 9 > 4 이므로 삽입
+ * 6: 4 < 6 이므로 4 pop, 9는 9 > 6 이므로 pass
+ */
