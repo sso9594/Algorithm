db24829c1da45975a2b3931c4c9c4cc82b28e5ab
syoung <58455389+sso9594@users.noreply.github.com>
2025-09-05 22:22:24 +0900

    [Gold IV] Title: 인구 이동, Time: 612 ms, Memory: 295980 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.java
+++ 백준/Gold/16234. 인구 이동/인구 이동.java
@@ -1,23 +1,26 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int N;
-    static int L;
-    static int R;
     static int[][] map;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        L = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+        int R = Integer.parseInt(st.nextToken());
 
         map = new int[N][N];
+        int answer = 0;
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
@@ -25,76 +28,64 @@ public class Main {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-
-        // 인구이동 한 국가인지 아닌 국가인지를 체크하기 위한한
-        boolean[][] visited = new boolean[N][N];
-        int count = 0;
-
-        while (isOpenBorderline(visited)) {          
-            count++;
-            visited = new boolean[N][N];
-        }
         
-        System.out.println(count);
-    }
-    
-    static boolean isOpenBorderline(boolean[][] visited){
-        boolean isOpen = false;
-
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < N; j++) {
-                boolean isValid = false;
-                if(!visited[i][j]){
-                    isValid = bfs(i, j, visited);
+        while (true) {
+            boolean[][] visited = new boolean[N][N];
+            boolean isValid = false;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if(!visited[i][j]){
+                        if (bfs(i, j, N, L, R, visited)) {
+                            isValid = true;
+                        }
+                    }
                 }
-                if(isValid) isOpen = true;
             }
+
+            if (!isValid) break;
+            answer++;
         }
 
-        return isOpen;
+        System.out.println(answer);
     }
 
-    static boolean bfs(int x, int y, boolean[][] visited){
-        Queue<int[]> queue = new LinkedList<>();
-        Set<int[]> set = new HashSet<>();
+    static boolean bfs(int x, int y, int N, int L, int R, boolean[][] visited){
+        Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{x, y});
         visited[x][y] = true;
-
-        boolean doBfs = false;
-
+        boolean isValid = false;
+        List<int[]> list = new ArrayList<>();
         int sum = 0;
         int count = 0;
 
         while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int currentX = current[0];
-            int currentY = current[1];
-            sum += map[currentX][currentY];
-            set.add(new int[]{currentX, currentY});
+            int[] cur = queue.poll();
+            int cx = cur[0];
+            int cy = cur[1];
+
+            list.add(new int[]{cx, cy});
+            sum += map[cx][cy];
             count++;
+
             for (int i = 0; i < dx.length; i++) {
-                int nextX = currentX + dx[i];
-                int nextY = currentY + dy[i];
-                if(nextX>=0 && nextY>=0 && nextX<N && nextY<N && !visited[nextX][nextY] &&
-                Math.abs(map[currentX][currentY] - map[nextX][nextY]) >= L &&
-                Math.abs(map[currentX][currentY] - map[nextX][nextY]) <= R){
-                    queue.offer(new int[]{nextX, nextY});
-                    visited[nextX][nextY] = true;
-                    doBfs = true;
+                int nx = cx + dx[i];
+                int ny = cy + dy[i];
+
+                if(nx >=0 && nx<N && ny>=0 && ny<N && Math.abs(map[cx][cy] - map[nx][ny]) >= L
+                && Math.abs(map[cx][cy] - map[nx][ny]) <= R && !visited[nx][ny]){
+                    queue.offer(new int[]{nx,ny});
+                    visited[nx][ny] = true;
+                    isValid = true;
                 }
             }
         }
 
-        if(doBfs){
-            int divide = sum / count;
+        int num = sum / count;
 
-            for (int[] is : set) {
-                map[is[0]][is[1]] = divide;
-            }
-            return true;
-        } else{
-            return false;
+        for (int[] is : list) {
+            map[is[0]][is[1]] = num;
         }
-        
+
+        return isValid;
     }
 }
