6b8a4885b1680d25f1d05e89cdba90039f6ef739
syoung <58455389+sso9594@users.noreply.github.com>
2025-09-10 23:29:51 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 168 ms, Memory: 18668 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
 import java.util.Arrays;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -9,69 +8,66 @@ import java.util.StringTokenizer;
 
 public class Main {
     static class Node implements Comparable<Node>{
-        int x, y, w;
-
-        Node(int x, int y, int w){
+        int x, y, val;
+        Node (int x, int y, int val){
             this.x = x;
             this.y = y;
-            this.w = w;
+            this.val = val;
         }
 
         @Override
-        public int compareTo(Main.Node o) {
-            return Integer.compare(this.w, o.w);
+        public int compareTo(Node o) {
+            return Integer.compare(this.val, o.val);
         }
     }
-    static int N;
-    static int[][] map;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int test_case = 0;
+        int tc = 1;
         while (true) {
-            N = Integer.parseInt(br.readLine());
-            if(N==0) break;
-            map = new int[N][N];
+            int N = Integer.parseInt(br.readLine());
+
+            if (N==0) {
+                break;
+            }
+    
+            int[][] map = new int[N][N];
+    
             for (int i = 0; i < N; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
                     map[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
-
-            int result = daijkstra(0,0);
-
-            System.out.println("Problem " + ++test_case +": "+ result);
-        }
-    }
-
-    static int daijkstra(int x, int y) {
-        PriorityQueue<Node> queue = new PriorityQueue<>();
-        queue.offer(new Node(x, y, map[x][y]));
-        int[][] dist = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        dist[x][y] = map[x][y];
-
-        while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            int cx = cur.x;
-            int cy = cur.y;
-
-            for (int i = 0; i < dx.length; i++) {
-                int nx = cx + dx[i];
-                int ny = cy + dy[i];
-
-                if(nx>=0 && ny>=0 && nx<N && ny<N &&
-                dist[nx][ny] > dist[cx][cy] + map[nx][ny]){
-                    dist[nx][ny] = dist[cx][cy] + map[nx][ny];
-                    queue.offer(new Node(nx, ny, map[nx][ny]));
+    
+            PriorityQueue<Node> pq = new PriorityQueue<>();
+            pq.offer(new Node(0, 0, map[0][0]));
+            int[][] dist = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                Arrays.fill(dist[i], Integer.MAX_VALUE);
+            }
+            dist[0][0] = map[0][0];
+    
+            int[] dx = {-1,1,0,0};
+            int[] dy = {0,0,-1,1};
+    
+            while (!pq.isEmpty()) {
+                Node cur = pq.poll();
+                int cx = cur.x;
+                int cy = cur.y;
+                int val = cur.val;
+                
+                for (int i = 0; i < dx.length; i++) {
+                    int nx = cx + dx[i];
+                    int ny = cy + dy[i];
+                    if(nx>=0 && nx<N && ny>=0 && ny<N &&
+                    dist[nx][ny] > dist[cx][cy] + map[nx][ny]){
+                        dist[nx][ny] = dist[cx][cy] + map[nx][ny];
+                        pq.offer(new Node(nx, ny, val+map[nx][ny]));
+                    }
                 }
             }
+    
+            System.out.println("Problem "+ tc++ + ": " + dist[N-1][N-1]);
         }
-
-        return dist[N-1][N-1];
     }
 }
