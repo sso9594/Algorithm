16fca517221bf340a2bac5ece7a1c2eaec0c72b3
syoung <58455389+sso9594@users.noreply.github.com>
2025-09-30 21:18:31 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 748 ms, Memory: 196104 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -6,10 +6,13 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N, M;
-    static int[][] map;
+
+    static int N;
+    static int M;
     static int[] dx = {-1,1,0,0};
     static int[] dy = {0,0,-1,1};
+    static int[][] map;
+
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -26,46 +29,48 @@ public class Main {
             }
         }
 
-        int result = bfs(0,0);
-
-        System.out.println(result);
+        bfs(0, 0);
     }
-    static int bfs(int x, int y) {
+
+    static void bfs(int x, int y){
         Queue<int[]> queue = new ArrayDeque<>();
         queue.offer(new int[]{x, y, 0, 1});
         boolean[][][] visited = new boolean[N][M][2];
         visited[x][y][0] = true;
+        
+        boolean isValid = false;
+
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int cx = cur[0];
             int cy = cur[1];
-            int count = cur[2];
-            int dist = cur[3];
+            int dis = cur[2];
+            int breakCnt = cur[3];
 
             if(cx == N-1 && cy == M-1){
-                return dist;
+                System.out.println(dis+1);
+                isValid = true;
+                break;
             }
 
             for (int i = 0; i < dx.length; i++) {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
-                // 일반 이동
-                if(nx>=0 && ny>=0 && nx<N && ny<M && !visited[nx][ny][count]
-                && map[nx][ny] == 0){
-                    queue.offer(new int[]{nx, ny, count, dist+1});
-                    visited[nx][ny][count] = true;
+                if(nx >=0 && nx<N && ny>=0 && ny<M 
+                && !visited[nx][ny][breakCnt] && map[nx][ny]==0){
+                    queue.offer(new int[]{nx, ny, dis+1, breakCnt});
+                    visited[nx][ny][breakCnt] = true;
                 }
-                //벽 부수고 이동
-                if(count < 1){
-                    if(nx>=0 && ny>=0 && nx<N && ny<M && !visited[nx][ny][count+1]
-                    && map[nx][ny] == 1){
-                        queue.offer(new int[]{nx, ny, count+1, dist+1});
-                        visited[nx][ny][count+1] = true;
-                    }
+                if(nx >=0 && nx<N && ny>=0 && ny<M 
+                && !visited[nx][ny][breakCnt] && map[nx][ny]==1 && breakCnt > 0){
+                    queue.offer(new int[]{nx, ny, dis+1, breakCnt-1});
+                    visited[nx][ny][breakCnt] = true;
                 }
             }
         }
 
-        return -1;
+        if(!isValid){
+            System.out.println(-1);
+        }
     }
 }
