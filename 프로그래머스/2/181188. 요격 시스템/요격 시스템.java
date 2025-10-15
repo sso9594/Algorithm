67ddd8e4dbd4deb7be98d74f5a681e7d4ef7de14
syoung <58455389+sso9594@users.noreply.github.com>
2025-10-15 23:02:05 +0900

    [level 2] Title: 요격 시스템, Time: 516.66 ms, Memory: 174 MB -BaekjoonHub

--- 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.java
@@ -3,15 +3,16 @@ import java.util.*;
 class Solution {
     public int solution(int[][] targets) {
         int answer = 0;
-        Arrays.sort(targets, (a, b) -> Integer.compare(a[1], b[1]));
-        int prefix = targets[0][1];
-        for(int i=1; i<targets.length; i++){
+        Arrays.sort(targets, (a, b) -> Integer.compare(a[1],b[1]));
+        
+        int prefix = -1;
+        
+        for(int i=0; i<targets.length; i++){
             if(prefix <= targets[i][0]){
                 answer++;
                 prefix = targets[i][1];
-            }        
+            }
         }
-        answer++;
         return answer;
     }
 }
