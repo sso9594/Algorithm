2b60d2df20168164388aa063d6b797fcd5359be2
syoung <58455389+sso9594@users.noreply.github.com>
2025-12-01 15:28:38 +0900

    [level 3] Title: 아이템 줍기, Time: 2.49 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/README.md
+++ 프로그래머스/3/87694. 아이템 줍기/README.md
@@ -1,10 +1,10 @@
 # [level 3] 아이템 줍기 - 87694 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/87694?language=java) 
 
 ### 성능 요약
 
-메모리: 73.4 MB, 시간: 0.71 ms
+메모리: 83.8 MB, 시간: 2.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 11월 22일 00:44:32
+2025년 12월 01일 15:28:34
 
 ### 문제 설명
 
--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -4,54 +4,56 @@ class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
         int[][] map = new int[101][101];
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        for(int[] square : rectangle){
-            int sx = square[0] * 2;
-            int sy = square[1] * 2;
-            int ex = square[2] * 2;
-            int ey = square[3] * 2;
+        
+        for(int[] point : rectangle){
+            int sX = point[0] * 2;
+            int sY = point[1] * 2;
+            int eX = point[2] * 2;
+            int eY = point[3] * 2;
             
-            // 가로 색칠
-            for(int i=sx; i<=ex; i++){
-                map[sy][i] = 1;
-                map[ey][i] = 1;
-            }
-            // 세로 색칠
-            for(int i=sy; i<=ey; i++){
-                map[i][sx] = 1;
-                map[i][ex] = 1;
+            for(int i=sX; i<=eX; i++){
+                map[sY][i] = 1;
+                map[eY][i] = 1;
             }
             
+            for(int i=sY; i<=eY; i++){
+                map[i][sX] = 1;
+                map[i][eX] = 1;
+            }
         }
         
-        // 내부 비우기
-        for(int[] square : rectangle){
-            int sx = square[0] * 2;
-            int sy = square[1] * 2;
-            int ex = square[2] * 2;
-            int ey = square[3] * 2;
-            
-            for(int i=sy+1; i<ey; i++){
-                for(int j=sx+1; j<ex; j++){
-                    map[i][j] = 0;
+        for(int i=0; i<101; i++){
+            for(int j=0; j<101; j++){
+                for(int[] point : rectangle){
+                    int sX = point[0] * 2;
+                    int sY = point[1] * 2;
+                    int eX = point[2] * 2;
+                    int eY = point[3] * 2;
+                    
+                    if(j > sX && j < eX && i > sY
+                      && i < eY){
+                        map[i][j] = 0;
+                    }
                 }
             }
-            
         }
         
         Queue<int[]> queue = new ArrayDeque<>();
-        queue.offer(new int[]{characterX * 2, characterY * 2, 0});
+        queue.offer(new int[]{characterY*2, characterX*2, 0});
         boolean[][] visited = new boolean[101][101];
-        visited[characterY * 2][characterX * 2] = true;
+        visited[characterY*2][characterX*2] = true;
+        
+        int[] dx = {-1,1,0,0};
+        int[] dy = {0,0,-1,1};
         
         while(!queue.isEmpty()){
             int[] cur = queue.poll();
-            int cx = cur[0];
-            int cy = cur[1];
-                        
+            int cy = cur[0];
+            int cx = cur[1];
+            int cnt = cur[2];
+            
             if(cx==itemX*2 && cy==itemY*2){
-                answer = cur[2]/2;
+                answer = cnt/2;
                 break;
             }
             
@@ -59,13 +61,15 @@ class Solution {
                 int nx = cx + dx[i];
                 int ny = cy + dy[i];
                 
-                if(nx>=0 && nx<101 && ny>=0 && ny<101 && map[ny][nx]==1 && !visited[ny][nx]){
-                    queue.offer(new int[]{nx,ny,cur[2]+1});
+                if(nx>=0 && nx<101 && ny>=0 && ny<101
+                  && map[ny][nx]==1 && !visited[ny][nx]){
+                    queue.offer(new int[]{ny, nx, cnt+1});
                     visited[ny][nx] = true;
                 }
             }
         }
         
+
         return answer;
     }
 }
