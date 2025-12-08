1c4f11804fd2fb47919a8da391cb1e77128a1427
syoung <58455389+sso9594@users.noreply.github.com>
2025-12-08 23:20:52 +0900

    [level 2] Title: 기능개발, Time: 0.37 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 0.25 ms
+메모리: 78.8 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 03일 17:37:47
+2025년 12월 08일 23:20:49
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -2,29 +2,35 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        List<Integer> result = new ArrayList<>();
-        Queue<Integer> queue = new ArrayDeque<>();
+        List<Integer> answer = new ArrayList<>();
+        int time = 0;
+        int idx = 0;
+        int cnt = 0;
         for(int i=0; i<progresses.length; i++){
-            queue.offer(progresses[i]);
-        }
-        int time = 1;
-        int index = 0;
-        while(!queue.isEmpty()){
-            if(!queue.isEmpty() && queue.peek() + speeds[index]*time >= 100){
-                int count = 0;
-                while(!queue.isEmpty() && queue.peek() + speeds[index]*time >= 100){
-                    queue.poll();
-                    index++;
-                    count++;
-                }
-                result.add(count);
+            int remain = 100 - progresses[i];
+            int ness = (int) Math.ceil((float)remain / (float)speeds[i]);
+            if (i==0){
+                cnt++;
+                time = ness;
+                continue;
             }
-            time++;
+            
+            if(time >= ness){
+                cnt++;
+            } else {
+                answer.add(cnt);
+                time = ness;
+                cnt = 1;
+            }
         }
-        int[] answer = new int[result.size()];
-        for(int i=0; i<result.size(); i++){
-            answer[i] = result.get(i);
+        
+        answer.add(cnt);
+        
+        int[] ans = new int[answer.size()];
+        for(int i=0; i<answer.size(); i++){
+            ans[i] = answer.get(i);
         }
-        return answer;
+        
+        return ans;
     }
 }
