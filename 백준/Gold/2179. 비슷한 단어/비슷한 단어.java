ac7c69205e688e1d84bdadc9a8d449af2c679cd6
syoung <58455389+sso9594@users.noreply.github.com>
2026-01-15 15:41:47 +0900

    [Gold IV] Title: 비슷한 단어, Time: 1736 ms, Memory: 16032 KB -BaekjoonHub

--- 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
+++ 백준/Gold/2179. 비슷한 단어/비슷한 단어.java
@@ -1,62 +1,53 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String[] words = new String[N];
+        String[] arr = new String[N];
 
         for (int i = 0; i < N; i++) {
-            words[i] = br.readLine();
+            arr[i] = br.readLine();
         }
 
-        int maxLen = 0;
-        int answerS = -1;
-        int answerT = -1;
+        int max = 0;
+        int aS = -1;
+        int aT = -1;
 
-        // 모든 쌍을 비교 (i < j 조건으로 중복 제거)
         for (int i = 0; i < N; i++) {
             for (int j = i + 1; j < N; j++) {
-                // 같은 문자열은 제외
-                if (words[i].equals(words[j])) continue;
+                if(arr[i].equals(arr[j])) continue;
 
-                // 공통 접두사 길이 계산
-                int len = getCommonPrefixLength(words[i], words[j]);
+                int len = compare(arr[i], arr[j]);
 
-                // 갱신 조건:
-                // 1. 더 긴 접두사를 찾았거나
-                // 2. 같은 길이이고 len > 0 이며, 첫 발견이거나 더 앞선 쌍인 경우
-                if (len > maxLen) {
-                    maxLen = len;
-                    answerS = i;
-                    answerT = j;
-                } else if (len == maxLen && len > 0) {
-                    if (answerS == -1 || i < answerS || (i == answerS && j < answerT)) {
-                        answerS = i;
-                        answerT = j;
+                if (len > max) {
+                    max = len;
+                    aS = i;
+                    aT = j;
+                } else if (len == max && len > 0) {
+                    if (aS == -1 || i < aS || (i == aS && j < aT)) {
+                        aS = i;
+                        aT = j;
                     }
                 }
             }
         }
 
-        System.out.println(words[answerS]);
-        System.out.println(words[answerT]);
+        System.out.println(arr[aS]);
+        System.out.println(arr[aT]);
     }
 
-    // 두 문자열의 공통 접두사 길이 반환
-    static int getCommonPrefixLength(String s1, String s2) {
+    static int compare(String s1, String s2){
         int len = Math.min(s1.length(), s2.length());
-        int count = 0;
-
-        for (int i = 0; i < len; i++) {
-            if (s1.charAt(i) == s2.charAt(i)) {
-                count++;
-            } else {
-                break;
-            }
+        int idx = 0;
+        while (idx < len && s1.charAt(idx) == s2.charAt(idx)){
+            idx++;
         }
 
-        return count;
+        return idx;
     }
+
 }
