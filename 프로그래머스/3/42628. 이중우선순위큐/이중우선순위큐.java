ce04a8fd184e8dce0a6800216845582ca8101d2d
syoung <58455389+sso9594@users.noreply.github.com>
2026-03-13 18:54:26 +0900

    [level 3] Title: 이중우선순위큐, Time: 98.44 ms, Memory: 137 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -3,40 +3,44 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
         int[] answer = new int[2];
-        PriorityQueue<Integer> maxq = new PriorityQueue<>((e1, e2) -> Integer.compare(e2, e1));
         PriorityQueue<Integer> minq = new PriorityQueue<>();
+        PriorityQueue<Integer> maxq = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
         
-        for(String op : operations){
-            String[] commands = op.split(" ");
-            String command = commands[0];
-            int num = Integer.parseInt(commands[1]);
+        for(String operation : operations){
+            String[] ops = operation.split(" ");
+            String op = ops[0];
+            int num = Integer.parseInt(ops[1]);
             
-            if(command.equals("I")){
-                maxq.offer(num);
-                minq.offer(num);
-            } else if(command.equals("D")){
-                if(num == 1){
-                    if(!maxq.isEmpty()){
-                        minq.remove(maxq.poll());
+            switch(op){
+                case "I":
+                    maxq.offer(num);
+                    minq.offer(num);
+                    break;
+                case "D":
+                    if(num == -1){
+                        if(!minq.isEmpty()){
+                            int min = minq.poll();
+                            maxq.remove(min);
+                        }
+                    } else{
+                        if(!maxq.isEmpty()){
+                            int max = maxq.poll();
+                            minq.remove(max);
+                        }
                     }
-                } else if(num == -1){
-                    if(!minq.isEmpty()){
-                        maxq.remove(minq.poll());    
-                    }
-                }
+                    break;
+                default:
+                    break;
             }
         }
         
-        if(maxq.isEmpty() && minq.isEmpty()){
+        if(maxq.isEmpty()){
             answer[0] = 0;
             answer[1] = 0;
-            
             return answer;
         }
-        
-        answer[0] = maxq.poll();
-        answer[1] = minq.poll();
-        
+        answer[0] = maxq.peek();
+        answer[1] = minq.peek();
         return answer;
     }
 }
