f34da2d46c0685e15fada7b727aaefd761ea4fae
syoung <58455389+sso9594@users.noreply.github.com>
2026-03-13 18:26:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 4.03 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,32 +1,43 @@
 import java.util.*;
 
 class Solution {
+    class Task {
+        int s, l;
+        Task(int s, int l){
+            this.s = s;
+            this.l = l;
+        }
+    }
     public int solution(int[][] jobs) {
         int answer = 0;
-        Arrays.sort(jobs, Comparator.comparingInt(a -> a[0]));
-        PriorityQueue<int[]> queue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
         
-        int time = 0;
-        int idx = 0;
-        int count = jobs.length;
+        PriorityQueue<Task> sq = new PriorityQueue<>((a, b) -> Integer.compare(a.s, b.s));
+        PriorityQueue<Task> lq = new PriorityQueue<>((a, b) -> Integer.compare(a.l, b.l));
         
-        while(idx < count || !queue.isEmpty()){
-            // 현재 시간에 요청이 되어있는 작업을 대기 큐에 넣음
-            while(idx < count && jobs[idx][0] <= time){
-                queue.offer(jobs[idx++]);
-            }
-            
-            // 작업이 있으면 하나만 처리
-            if(!queue.isEmpty()){
-                int[] task = queue.poll();
-                time += task[1];
-                answer += time - task[0];
-            // 작업이 없으면 타임 점프
-            } else{
-                time = jobs[idx][0];
-            }
+        for(int[] job : jobs){
+            Task t = new Task(job[0], job[1]);
+            sq.offer(t);
         }
         
-        return answer / count;
+        int t = sq.peek().s;
+        
+        while(!sq.isEmpty() || !lq.isEmpty()){
+            while(!sq.isEmpty() && sq.peek().s <= t){
+                lq.offer(sq.poll());
+            }
+            if(lq.isEmpty()){
+                t = sq.peek().s;
+                continue;
+            }
+            Task task = lq.poll();
+            t += task.l;
+            answer += t - task.s;
+        }
+
+        return answer / jobs.length;
     }
 }
+
+/**
+소요시간 > 요청시각 > 작업 번호
+*/
