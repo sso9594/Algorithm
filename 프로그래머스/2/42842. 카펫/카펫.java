12dc271038d334167b4b846710b6051a0a657ba1
syoung <58455389+sso9594@users.noreply.github.com>
2026-03-14 16:58:12 +0900

    [level 2] Title: 카펫, Time: 0.12 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -3,33 +3,38 @@ import java.util.*;
 class Solution {
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
+        int sum = brown + yellow;
         List<Integer> list = new ArrayList<>();
-        // yellow의 약수 구하기
-        for(int i=1; i<=Math.sqrt(yellow); i++){
-            if(yellow%i==0) list.add(i);
+        for(int i=1; i<=Math.sqrt(sum); i++){
+            if(sum % i == 0){
+                list.add(i);
+            }
         }
-        // 곱해서 yellow가 나오는 약수 묶음 구하기
-        // 두 수 * 2 - 4 = brown이 나오는 값 구하기
-        for(int i=0; i<list.size(); i++){
-            int num1 = list.get(i);
-            int num2 = yellow / num1;
-            
-            int cal = (num1 + num2+4) * 2 - 4;
-            if(cal == brown){
-                answer[0] = Math.max(num1+2, num2+2);
-                answer[1] = Math.min(num1+2, num2+2);
+        
+        for(int num1 : list){
+            int num2 = sum / num1;
+            if(num1-2 > 0 && num2-2 > 0){
+                num1 -= 2;
+                num2 -= 2;
+                
+                int mul = num1 * num2;
+                if(yellow == mul){
+                    answer[0] = Math.max(num1+2, num2+2);
+                    answer[1] = Math.min(num1+2, num2+2);
+                }
             }
         }
         return answer;
     }
 }
-/*
-1 2 3 4 6 8 12 24
 
-3 26 => 29 * 2 - 4 = 54
-4 14 => 18 * 2 - 4 = 32
-5 10 => 15 * 2 - 4 = 26 
-6 8 =>  14 * 2 - 4 = 24 => brown
+/**
+12의 약수
+1, 2, 3, 4, 6, 12
+(1, 12), (2, 6), (3, 4)
 
-
+48의 약수
+(1, 48), (2, 24), (3, 16), (4, 12), (6, 8)
+1 14 / 2 10 / 4 6
+14 20 24 == yellow
 */
