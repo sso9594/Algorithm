b0034fb0adff1eff9c3b9a3748a7650ce255367f
syoung <58455389+sso9594@users.noreply.github.com>
2026-04-08 03:34:20 +0900

    [level 3] Title: 입국심사, Time: 70.22 ms, Memory: 99 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,20 +1,22 @@
 class Solution {
     public long solution(int n, int[] times) {
-        long answer = 0;
-        long right = 1_000_000_000L * 1_000_000_000L;
+        long answer = Long.MAX_VALUE;
         long left = 0;
-        while(left <= right){
-            long mid = (right + left) / 2;
-            long sum = 0;
-            for(int time : times){
-                sum += mid / time;
+        long right = 1_000_000_000L * 1_000_000_000L;
+        while(left <= right) {
+            long mid = (left + right) / 2;
+            long count = 0;
+            for(int i=0; i<times.length; i++){
+                count += mid / times[i];
             }
-            if(sum>=n){
+            
+            if(count >= n){
                 answer = mid;
                 right = mid - 1;
             } else {
                 left = mid + 1;
             }
+            
         }
         return answer;
     }
