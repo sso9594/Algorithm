e445fdcc12367493aae848a614e9b127fd15ca30
syoung <58455389+sso9594@users.noreply.github.com>
2026-05-13 16:44:33 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 61.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,54 @@
+/**
+1. 하나의 알파벳만 바꿀 수 있음
+2. words 목록에 있는 단어만 바꿀 수 있음
+
+begin -> target까지의 거리를 구하시오 (불가능 시 0 출력)
+*/
 import java.util.*;
 
 class Solution {
-    class Node{
-        String token;
+    class Node {
+        String str;
         int count;
-        Node(String token, int count){
-            this.token = token;
+        
+        Node(String str, int count){
+            this.str = str;
             this.count = count;
         }
     }
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
+        
         Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(begin, 0));
+        queue.add(new Node(begin, 0));
         boolean[] visited = new boolean[words.length];
+        
         while(!queue.isEmpty()){
             Node cur = queue.poll();
-            String curStr = cur.token;
-            int cCnt = cur.count;
+            String curStr = cur.str;
+            int count = cur.count;
+                        
             if(curStr.equals(target)){
-                return cCnt;
+                answer = count;
+                break;
             }
+            
             for(int i=0; i<words.length; i++){
-                if(!visited[i] && canConvert(words[i], curStr)){
-                    queue.offer(new Node(words[i], cCnt+1));
+                int nxtCnt = count + 1;
+                if(!visited[i] && canConvert(curStr, words[i])){
+                    queue.offer(new Node(words[i], nxtCnt));
                     visited[i] = true;
                 }
             }
         }
-        return 0;
+                                
+        return answer;
     }
     
-    boolean canConvert(String token1, String token2){
+    public boolean canConvert(String str1, String str2){
         int count = 0;
-        for(int i=0; i<token1.length(); i++){
-            if(token1.charAt(i) != token2.charAt(i)){
+        for(int i=0; i<str1.length(); i++){
+            if(str1.charAt(i) != str2.charAt(i)){
                 count++;
             }
         }
