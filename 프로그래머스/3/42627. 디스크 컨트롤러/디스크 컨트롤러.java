bdf019e24b3b34ef9335ef5eb4d3d41f3462e4d2
syoung <58455389+sso9594@users.noreply.github.com>
2026-05-13 16:19:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.59 ms, Memory: 60 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,43 +1,59 @@
+/**
+한번에 하나의 작업만 수행 가능
+[작업번호, 요청시각, 소요시간]
+우선순위: 소요시간 > 요청시각 > 번호
+비선점
+*/
 import java.util.*;
 
 class Solution {
     class Task {
-        int s, l;
-        Task(int s, int l){
-            this.s = s;
-            this.l = l;
+        int id, start, time;
+        Task (int id, int start, int time){
+            this.id = id;
+            this.start = start;
+            this.time = time;
+        }
+        
+        @Override
+        public String toString() {
+            return id + " " + start + " " + time;
         }
     }
     public int solution(int[][] jobs) {
         int answer = 0;
-        
-        PriorityQueue<Task> sq = new PriorityQueue<>((a, b) -> Integer.compare(a.s, b.s));
-        PriorityQueue<Task> lq = new PriorityQueue<>((a, b) -> Integer.compare(a.l, b.l));
-        
-        for(int[] job : jobs){
-            Task t = new Task(job[0], job[1]);
-            sq.offer(t);
+        PriorityQueue<Task> startPQ = new PriorityQueue<>(
+            (a, b) -> Integer.compare(a.start, b.start)
+        );
+        PriorityQueue<Task> timePQ = new PriorityQueue<>(new Comparator<Task>(){
+            @Override
+            public int compare(Task t1, Task t2){
+                if(t1.time != t2.time){
+                    return Integer.compare(t1.time, t2.time);
+                } else {
+                    return Integer.compare(t1.id, t2.id);
+                }
+            }
+        });
+        for(int i=0; i<jobs.length; i++){
+            startPQ.add(new Task(i, jobs[i][0], jobs[i][1]));
         }
         
-        int t = sq.peek().s;
+        int time = 0;
         
-        while(!sq.isEmpty() || !lq.isEmpty()){
-            while(!sq.isEmpty() && sq.peek().s <= t){
-                lq.offer(sq.poll());
+        while(!startPQ.isEmpty() || !timePQ.isEmpty()){
+            while(!startPQ.isEmpty() && startPQ.peek().start <= time){
+                timePQ.add(startPQ.poll());
             }
-            if(lq.isEmpty()){
-                t = sq.peek().s;
-                continue;
+            if(!timePQ.isEmpty()){
+                Task task  = timePQ.poll();
+                time += task.time;
+                answer += time - task.start;
+            } else {
+                time++;
             }
-            Task task = lq.poll();
-            t += task.l;
-            answer += t - task.s;
         }
-
+        
         return answer / jobs.length;
     }
 }
-
-/**
-소요시간 > 요청시각 > 작업 번호
-*/
