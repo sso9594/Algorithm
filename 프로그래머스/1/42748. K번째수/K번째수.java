cd5f89f61b72c8598d2c9694f3d09bb8d6052718
syoung <58455389+sso9594@users.noreply.github.com>
2026-08-23 21:33:49 +0900

    [level 1] Title: K번째수, Time: 2.02 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -3,18 +3,15 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
-        int n = 0;
-        //commands의 갯수 만큼 반복하는 반복문
+        int idx = 0;
         for(int[] command : commands){
-            // 자른 array를 담을 배열 초기화
-            List<Integer> cutArray = new ArrayList<>();
-            //array를 자르는 알고리즘
-            for(int i=command[0]-1; i<=command[1]-1; i++){
-                cutArray.add(array[i]);
+            int size = command[1]-command[0]+1;
+            int[] newArr = new int[size];
+            for (int i = command[0], j = 0; i <= command[1]; i++, j++) {
+                newArr[j] = array[i - 1]; 
             }
-            Collections.sort(cutArray);
-            answer[n] = cutArray.get(command[2]-1);
-            n++;
+            Arrays.sort(newArr);
+            answer[idx++] = newArr[command[2]-1];
         }
         return answer;
     }
